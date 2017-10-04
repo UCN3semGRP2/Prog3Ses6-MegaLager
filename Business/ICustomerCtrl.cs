4fdf7c6715f28d221f8aa4e2528bb755cb34e4a3
Anders <a.jensen1996@gmail.com>
2017-10-04 13:17:44 +0200

    Created ICustomerCtrl class

---
+++ Business/ICustomerCtrl.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public interface ICustomerCtrl
+    {
+        Customer CreateCustomer(String name, String phone, String customerNo, String address, String zip, String accountNo);
+        Customer FindCustomer(String customerNo);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
+        
+    }
+}
