e2d168a2bd146411b0c6f5575a2a8140298b589c
Jesper Jørgensen <jesperbroge@gmail.com>
2017-10-04 12:19:10 +0200

    DataAccess: added a db class

---
+++ DataAccess/db.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class db
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public SqlConnection SqlConnection()
+        {
+            var conn = new SqlConnection(this.connectionString);
+            return conn;
+        }
+    }
+}
