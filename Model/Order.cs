0452347f951886e5912d1da7eb548d74a0814da3
Anders <a.jensen1996@gmail.com>
2017-10-04 10:35:11 +0200

    Created Order and State class

---
+++ Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public State State { get; set; }
+
+    }
+}
