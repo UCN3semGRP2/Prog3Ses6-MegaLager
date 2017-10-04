b0aadd3c1fbb62c1b7fbe6e9dcb32d562d6493aa
Anders <a.jensen1996@gmail.com>
2017-10-04 10:38:30 +0200

    Updateed modelProj

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Customer
+    public abstract class Customer
     {
+        public int Id { get; set; }
+        public int Phone { get; set; }
+        public int CustomerNo { get; set; }
+        public int Zip { get; set; }
+        public int AccountNo { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
     }
 }
