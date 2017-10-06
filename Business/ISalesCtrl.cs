0a35ce77f3a5c6a63af7fcd6f4dedd4a8a96230e
Rasmus Nedergaard <rasmusnedergaard@Rasmuss-MacBook-Pro.local>
2017-10-06 11:15:42 +0200

    Created ISalesCtrl.

---
+++ Business/ISalesCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Business
+{
+    interface ISalesCtrl
+    {
+        void CreateCustomer(String name, String phone, String customerNo, String address, String zip, String accountNo);
+        void AddCustomer(Customer customer);
+        void CreateOrder();
+        void UpdateOrder();
+        void AddOrderLine();
+
+    }
+}
