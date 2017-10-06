f8bb163d5b71eda7f46fbe02c7acd2e29bf03d30
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2017-10-06 14:04:46 +0200

    Created InventoryGUI project

---
+++ InventoryGUI/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WcfService;
+
+namespace InventoryGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static WcfService.IService1 proxy = new WcfService.Service1();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ProductGrid.ItemsSource = proxy.FindProductByName(""); // TODO FindAllProducts
+        }
+
+        private void OKBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("YO!");
+        }
+    }
+}
