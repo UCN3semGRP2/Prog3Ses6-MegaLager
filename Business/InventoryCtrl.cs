6004156e85cd1f07a13a77fa6dab9b1c01eca464
Niklas Jørgensen <niklas220587@hotmail.com>
2017-10-11 10:16:06 +0200

    Created the inventory controller

---
+++ Business/InventoryCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Business
+{
+    public class InventoryCtrl : IInventoryCtrl
+    {
+        private IProductCtrl productCtrl;
+        
+        public void CreateProduct(string name, double price, int supply, List<Copy> copies, int productNumber)
+        {
+            productCtrl.Create(name, price, supply); 
+            // TODO: product controller need to take more parameters
+
+        }
+
+        public IEnumerable<Product> FindProductByName(string name)
+        {
+            return productCtrl.FindByName(name);
+        }
+
+        public IEnumerable<Product> FindProductByProductNo(int productNo)
+        {
+            return productCtrl.FindByProductNo(productNo);
+        }
+
+        public IEnumerable<Product> GetAllSoldoutProducts()
+        {
+            return productCtrl.FindSoldOutProducts();
+        }
+
+        public void UpdatePrice(Product p, double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStock(Product p, int newSupply)
+        {
+            productCtrl.UpdateSupply(p, newSupply);
+        }
+    }
+}
