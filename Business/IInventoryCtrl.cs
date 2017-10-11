8bbf87c5d5226788a4e969b9320783ed12c2b197
Niklas Jørgensen <niklas220587@hotmail.com>
2017-10-11 10:03:58 +0200

    Modified the interface for the inventory controller to take the right parameters when creating a product

--- Business/IInventoryCtrl.cs
+++ Business/IInventoryCtrl.cs
@@ -9,7 +9,7 @@ namespace Business
 {
     public interface IInventoryCtrl
     {
-        void CreateItem();
+        void CreateProduct(string name, double price, int supply, List<Copy> copies, int productNumber);
         void UpdateStock(Product p, int newSupply);
         void UpdatePrice(Product p, double price);
         IEnumerable<Product> GetAllSoldoutProducts();
