a208e0cee95749be0d16a2b0131e8f85331ed2da
Niklas Jørgensen <niklas220587@hotmail.com>
2017-10-13 11:30:51 +0200

    Created customerSrevice and its interface

---
+++ WcfService/CusomterService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Business;
+
+namespace WcfService
+{
+    public class CusomterService : ICustomerService
+    {
+        private CustomerCtrl customerCtrl;
+        public Customer CreateComCustomer(string name, string phone, string address, string zip, string accountNo, string cvr, string ean)
+        {
+            return customerCtrl.CreateComCustomer(name, phone, address, zip, accountNo, cvr, ean);
+        }
+        public Customer CreatePrivCustomer(String name, String phone, String address, String zip, String accountNo)
+        {
+            return customerCtrl.CreatePrivateCustomer(name, phone, address, zip , accountNo);
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            customerCtrl.DeleteCustomer(customer);
+        }
+
+        public void UpdateCustomer(string name, string phone, string CustomerNo, string Zip, string AccountNo, string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WcfService/ICustomerService.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface ICustomerService
+    {
+        [OperationContract]
+        Customer CreateComCustomer(string name, string phone, string address, string zip, string accountNo, string cvr, string ean);
+        [OperationContract]
+        Customer CreatePrivCustomer(String name, String phone, String address, String zip, String accountNo);
+        [OperationContract]
+        void UpdateCustomer(string name, String phone, string CustomerNo, String Zip, String AccountNo, String address);
+        [OperationContract]
+        void DeleteCustomer(Customer customer);
+    }
+}
+
+
