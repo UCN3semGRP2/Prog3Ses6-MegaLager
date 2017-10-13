aba2e7cad439358ed21091fcf5cbe6c7f9424bbe
Niklas Jørgensen <niklas220587@hotmail.com>
2017-10-13 09:05:18 +0200

    Created the orderlinectrl

---
+++ Business/OrderLineCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Business
+{
+    public class OrderLineCtrl : IOrderLineCtrl
+    {
+        public OrderLine CreateOrderLine(int amount, Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrderLine(OrderLine orderLine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderLine FindOrderLineById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetSubTotal(IEnumerable<OrderLine> orderLines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
