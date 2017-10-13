11731b4fe3b9fe4af445adc9d0c347ce415d9132
Anders <a.jensen1996@gmail.com>
2017-10-13 08:40:10 +0200

    WIP

--- DataAccess/CustomerDb.cs
+++ DataAccess/CustomerDb.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using System.Data;
+using System.Transactions;
+using System.Data.SqlClient;
 
 namespace DataAccess
 {
-   public class CustomerDb : ICustomerDB
+    public class CustomerDb : ICustomerDB
     {
-        private static CustomerDb instance; 
+        private static CustomerDb instance;
+        private db db;
 
+        public CustomerDb()
+        {
+            db = db.GetInstance;
+            db.SqlConnection();
+        }
         public static CustomerDb GetInstance
         {
             get
@@ -25,7 +34,62 @@ namespace DataAccess
 
         public Customer Create(Customer entity)
         {
-            throw new NotImplementedException();
+            if (entity.GetType() == typeof(Priv))
+            {
+                entity = createPrivate(entity);
+            }
+            else if (entity.GetType() == typeof(Commercial))
+            {
+
+            }
+
+            return entity;
+        }
+
+        public Customer createPrivate(Customer entity)
+        {
+            TransactionOptions to = new TransactionOptions { IsolationLevel = System.Transactions.IsolationLevel.RepeatableRead };
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.RequiresNew, to))
+            {
+                using (SqlConnection connection = db.SqlConnection())
+                {
+                    connection.Open();
+                    int amountOfCustomers;
+                    using (SqlCommand cmd = connection.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT Count(*) FROM Customer WHERE customer_no == @customerNo";
+                        cmd.Parameters.AddWithValue("customerNo", entity.CustomerNo);
+                        amountOfCustomers = (int)cmd.ExecuteScalar();
+                    }
+                    if (amountOfCustomers == 0)
+                    {
+                        
+                        using (SqlCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = "INSERT INTO Customer (name, phone, customer_no, address, zip, account_no) VALUES(@name, @phone, @customer_no, @address, @zip, @account_no)";
+                            cmd.Parameters.AddWithValue("name", entity.Name);
+                            cmd.Parameters.AddWithValue("phone", entity.Phone);
+                            cmd.Parameters.AddWithValue("customerNo", entity.CustomerNo);
+                            cmd.Parameters.AddWithValue("address", entity.Address);
+                            cmd.Parameters.AddWithValue("zip", entity.Zip);
+                            cmd.Parameters.AddWithValue("accountNo", entity.Zip);
+                            entity.Id = (int)cmd.ExecuteScalar();
+                        }
+
+                        using (SqlCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = "INSERT INTO Private (customer_id) VALUES(@customer_id)";
+                            cmd.Parameters.AddWithValue("customer_id", entity.Id);
+                            cmd.ExecuteNonQuery();
+                        }
+
+
+                    }
+                }
+                scope.Complete();
+            }
+
+            return entity;
         }
 
         public void Delete(Customer entity)
@@ -50,7 +114,23 @@ namespace DataAccess
 
         public void Update(Customer entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = db.SqlConnection())
+            {
+                connection.Open();
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "UPDATE Customer SET name=@name, phone=@phone, customer_no=@customer_no, address=@address, zip=@zip, account_no=@account_no WHERE id=@id";
+                    cmd.Parameters.AddWithValue("name", entity.Name);
+                    cmd.Parameters.AddWithValue("phone", entity.Phone);
+                    cmd.Parameters.AddWithValue("customerNo", entity.CustomerNo);
+                    cmd.Parameters.AddWithValue("address", entity.Address);
+                    cmd.Parameters.AddWithValue("zip", entity.Zip);
+                    cmd.Parameters.AddWithValue("accountNo", entity.Zip);
+                    cmd.Parameters.AddWithValue("id", entity.Id);
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
         }
     }
 }
